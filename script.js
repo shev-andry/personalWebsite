de7e89490b05dccdefed732d927ac9c9a9609daa
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-08 12:56:55 +0700

    add new command and move all command func to 1 obj

--- script.js
+++ script.js
@@ -1,90 +1,96 @@
-const storage = () => {};
+const storage = () => {
+  let dir = "~";
+};
 
 const displayController = () => {
-  const body = document.querySelector("body");
+  const container = document.querySelector(".container");
   let input = document.querySelector(".prompt");
   let form = document.querySelector(".currentForm");
 
   function createPs1() {
-    const div = document.createElement('div')
-    const p = document.createElement('p')
+    const div = document.createElement("div");
+    const p = document.createElement("p");
     const spanUser = document.createElement("span");
     const spanHost = document.createElement("span");
     const spanDir = document.createElement("span");
-    const form = document.createElement('form')
-    const input = document.createElement('input')
-    spanUser.innerText = 'user'
-    spanHost.innerText = '@sevaaaDev'
-    spanDir.innerText = '[~]'
-    p.appendChild(spanUser)
-    p.appendChild(spanHost)
-    p.appendChild(spanDir)
-    p.append('$')
-    input.classList.add('prompt')
-    form.appendChild(input)
-    form.classList.add('currentForm')
-    div.classList.add('ps1')
-    div.appendChild(p)
-    div.appendChild(form)
-    body.appendChild(div)
+    const form = document.createElement("form");
+    const input = document.createElement("input");
+    spanUser.innerText = "user";
+    spanHost.innerText = "@sevaaaDev";
+    spanDir.innerText = "[~]";
+    p.appendChild(spanUser);
+    p.appendChild(spanHost);
+    p.appendChild(spanDir);
+    p.append("$");
+    input.classList.add("prompt");
+    form.appendChild(input);
+    form.classList.add("currentForm");
+    div.classList.add("ps1");
+    div.appendChild(p);
+    div.appendChild(form);
+    container.appendChild(div);
   }
 
   function enter() {
-    createPs1()
-    input.disabled = true
-    input.classList.remove('prompt')
-    input = document.querySelector('.prompt')
-    input.focus()
-    console.log(input)
-    form.classList.remove('currentForm')
-    form = document.querySelector('.currentForm')
-    console.log(form)
-  }
-
-  function ls() {
-    const dir = document.createElement("p");
-    dir.innerText = "tes";
-    body.appendChild(dir);
-    enter();
+    createPs1();
+    input.disabled = true;
+    input.classList.remove("prompt");
+    input = document.querySelector(".prompt");
+    input.focus();
+    console.log(input);
+    form.classList.remove("currentForm");
+    form = document.querySelector(".currentForm");
+    console.log(form);
   }
 
   function getInput() {
-    return input
+    return input;
   }
 
   function getForm() {
-    return form
+    return form;
   }
 
-  return { input, form, ls, enter, getInput, getForm };
+  let command = {
+    ls: () => {
+      const dir = document.createElement("p");
+      dir.innerText = "tes";
+      container.appendChild(dir);
+      enter();
+    },
+    cls: () => {
+      container.innerHTML = "";
+      enter();
+    },
+  };
+
+  return { enter, getInput, getForm, command };
 };
 
 const inputController = (() => {
   const display = displayController();
-  let form = display.getForm()
-  listenForm()
+  let form = display.getForm();
+  listenForm();
   window.addEventListener("click", () => {
-    input = display.getInput()
-    form = display.getForm()
+    input = display.getInput();
+    form = display.getForm();
     input.focus();
   });
 
   function submit(e) {
     e.preventDefault();
     checkInput();
-    form = display.getForm()
-    listenForm()
+    form = display.getForm();
+    listenForm();
   }
-  
+
   function listenForm() {
     form.addEventListener("submit", submit);
   }
-  
+
   function checkInput() {
-    if (display.input.value === "ls") {
-      display.ls();
-      form = display.getForm()
-      console.log(form)
-    }
+    input = display.getInput();
+    if (input.value === '') return display.enter()
+    display.command[input.value]()
   }
 })();
